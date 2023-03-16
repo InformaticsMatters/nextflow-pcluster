873f8fe2f18e366f93ec2490c783cd17fdb41891
Alan Christie <alan.christie@matildapeak.com>
2023-03-16 15:17:00 +0000

    Fix render-policies (and dds EVERYTHING)

--- render-policies.sh
+++ render-policies.sh
@@ -1,22 +1,18 @@
 #!/usr/bin/env bash
 
-if [ "$#" -ne 4 ]
+if [ "$#" -ne 2 ]
 then
   echo "ERROR: Wrong number of arguments"
-  echo "Usage: render-policies.sh AWS_REGION ACCOUNT_ID CLUSTER_NAME ROLE_NAME"
+  echo "Usage: render-policies.sh AWS_REGION ACCOUNT_ID"
   exit 1
 fi
 
 REGION=$1
 ACCOUNT=$2
-CLUSTER=$3
-ROLE=$4
 
-for F in instance operator user
+for F in head-node image-builder instance privileged
 do
   sed 's/<REGION>/'"$REGION"'/' iam/${F}-policy.json > a.json
-  sed 's/<AWS ACCOUNT ID>/'"$ACCOUNT"'/' a.json > b.json
-  sed 's/<PARALLELCLUSTER EC2 ROLE NAME>/'"$ROLE"'/' b.json > c.json
-  sed 's/<CLUSTERNAME>/'"$CLUSTER"'/' c.json > nf-${F}-policy.json
-  rm a.json b.json c.json
+  sed 's/<AWS ACCOUNT ID>/'"$ACCOUNT"'/' a.json > nf-${F}-policy.json
+  rm a.json
 done
